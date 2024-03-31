9cf1422bd6db7ec0ef2197e9d6d5df080c68e0c1
Sergiu Varga <sergiu.varga@student.upt.ro>
2024-03-31 18:03:36 +0000

    lupta cu outputul

--- Project/cristi.c
+++ Project/cristi.c
@@ -10,6 +10,61 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+typedef struct _file{
+
+    const char* filename;
+    long unsigned inode;
+    long unsigned filesize;
+    long unsigned hlinks;
+    char filetype;
+    const char* owner;
+    const char* group;
+}File; 
+
+void printFileInfo(File* f,int count,int fdOut) {
+
+       printf("ENTRY %d: filename => %s\n\t ",++count,f->filename);
+       printf("inode => %lu\t hardlinks => %lu\t size => %lu\n\t",f->inode,f->hlinks,f->filesize);
+       printf("TYPE=> %c OWNID: %s GRPID: %s\n",f->filetype,f->owner,f->group);
+
+    int w;
+    w = write(fdOut, &(f->filename), sizeof(f->filename));
+    if (w == -1) {
+        perror("writing filename to output file");
+        exit(errno);
+    }
+    w = write(fdOut, &(f->inode), sizeof(f->inode));
+    if (w == -1) {
+        perror("writing inode to output file");
+        exit(errno);
+    }
+    w = write(fdOut, &(f->filesize), sizeof(f->filesize));
+    if (w == -1) {
+        perror("writing filesize to output file");
+        exit(errno);
+    }
+    w = write(fdOut, &(f->hlinks), sizeof(f->hlinks));
+    if (w == -1) {
+        perror("writing hardlinks to output file");
+        exit(errno);
+    }
+    w = write(fdOut, &(f->filetype), sizeof(f->filetype));
+    if (w == -1) {
+        perror("writing filetype to output file");
+        exit(errno);
+    }
+    w = write(fdOut, &(f->owner), sizeof(f->owner));
+    if (w == -1) {
+        perror("writing owner to output file");
+        exit(errno);
+    }
+    w = write(fdOut, &(f->group), sizeof(f->group));
+    if (w == -1) {
+        perror("writing group to output file");
+        exit(errno);
+    }
+} 
+
 char getEntryType(mode_t m) {
 
     if( S_ISREG(m) ) return '-';
@@ -22,7 +77,7 @@ char getEntryType(mode_t m) {
     else return '?';
 }
 
-void parseDir(const char* dirName) {
+void parseDir(const char* dirName,int outputFile) {
 
     
     DIR* dir;
@@ -64,13 +119,20 @@ void parseDir(const char* dirName) {
             grp = getgrgid(statVar.st_gid);
             const char* group = (grp!=NULL) ? grp->gr_name : "UNKNOWN";
 
-                 printf("ENTRY %d: filename => %s\n\t ",++count,entry->d_name);
-                 printf("inode => %lu\t hardlinks => %lu\t size => %lu\n\t",statVar.st_ino,statVar.st_nlink,statVar.st_size);
-                 printf("TYPE=> %c OWNID: %s GRPID: %s\n",getEntryType(statVar.st_mode),owner,group);
+            File f;
+            f.filename = entry->d_name;
+            f.inode = statVar.st_ino;
+            f.filesize = statVar.st_size;
+            f.filetype = getEntryType(statVar.st_mode);
+            f.hlinks = statVar.st_nlink;
+            f.owner = owner;
+            f.group = group;
+
+            printFileInfo(&f,count++,outputFile);
 
             if( S_ISDIR(statVar.st_mode) ) {
                 printf("move to inner dir: \n");
-                parseDir( pathBuf );
+                parseDir( pathBuf, outputFile );
                 printf("return to parent dir:\n");
             }
     }
@@ -86,7 +148,7 @@ int main(int argc, char** argv) {
         exit(1);
     }
     
-    int outFile = open("output.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IREAD | S_IWRITE);
+    int outFile = open("output.bin", O_WRONLY | O_CREAT | O_TRUNC, S_IREAD | S_IWRITE);
 
     if( outFile == -1 ) {
         perror("opening file");
@@ -94,8 +156,7 @@ int main(int argc, char** argv) {
     }
 
     const char* parentDirName = argv[1];
-    parseDir(parentDirName);
-
+    parseDir(parentDirName,outFile);
 
     close(outFile);
     return 0;
